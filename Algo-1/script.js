3d5ff6db713ccd65563deb80aea16f8db0a2392b
gulovshe <gulovshafa@code.edu.az>
2024-07-19 19:57:47 +0400

    deyisiklik etdim

---
+++ Algo-1/script.js
@@ -0,0 +1,12 @@
+function ikireqemli(){
+    let sum=0
+    for(let i=10;i<100;i++){
+            if (i%2==1){
+            sum+=i
+    
+            }
+        } 
+        return sum
+    }
+    let res=ikireqemli()
+  console.log(res)
