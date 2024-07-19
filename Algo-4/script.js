38d62457eb2f750082c2e7db0c7f995f5793064b
gulovshe <gulovshafa@code.edu.az>
2024-07-19 21:15:49 +0400

    algo-4 etdim

---
+++ Algo-4/script.js
@@ -0,0 +1,14 @@
+let a=[1,2,3,4,5]
+
+ function cem(a){
+    // let a=[1,2,3,4,5]
+    // let sum=0
+    let sum=0
+    for(let i=0;i<a.length;i++){
+        sum+=a[i]
+         
+       
+    }
+    return sum
+ }
+ console.log(cem(a))
