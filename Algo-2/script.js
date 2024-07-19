9aa37ba9d337d8605efb3d52ec56d0b97a22bf8c
gulovshe <gulovshafa@code.edu.az>
2024-07-19 20:06:32 +0400

    algo-2

---
+++ Algo-2/script.js
@@ -0,0 +1,12 @@
+function ucreqemli(){
+    let count=0
+    for(let i=100;i<1000;i++){
+            if (i%2==0 && i%5==0){
+            count++
+    
+            }
+        } 
+        return count
+    }
+    let res=ucreqemli()
+  console.log(res)
