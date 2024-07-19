6449678781ef9c9f392f002f36348de91ed9d549
gulovshe <gulovshafa@code.edu.az>
2024-07-19 20:24:46 +0400

    algo-3 etdim

---
+++ Algo-3/script.js
@@ -0,0 +1,15 @@
+
+function ucreqemli(){
+    let sum=0
+     for(let i=100;i<1000;i++){
+            if (i%7==0 && i%8==0){
+            sum+=i
+    
+            }
+        } 
+        return sum
+
+    }
+    console.log(ucreqemli())
+
+ 
